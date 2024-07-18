f4ac853071f3b536ca26566eccd8820d40432cea
Thanh Phong <btphongdev@gmail.com>
2024-07-18 19:29:44 +0700

    fix window not found

--- loader.js
+++ loader.js
@@ -1,7 +1,17 @@
 'use client'
 
+import { useState, useEffect } from 'react'
+
 export default function myImageLoader({ src, width, quality }) {
-  const currentHost = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
+  const [currentHost, setCurrentHost] = useState('')
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const host = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
+
+      setCurrentHost(host)
+    }
+  }, [currentHost])
+  // const currentHost = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
 
   if (src.startsWith('https://images.pexels.com')) return src
   return `${currentHost}/${src}?w=${width}&q=${quality || 75}`
