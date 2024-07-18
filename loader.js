e24cd405e42fda1bba3f766fe979f4cf07c4955e
Thanh Phong <btphongdev@gmail.com>
2024-07-18 19:07:10 +0700

    config host

--- loader.js
+++ loader.js
@@ -1,7 +1,7 @@
 'use client'
 
 export default function myImageLoader({ src, width, quality }) {
-  const currentHost = `${window.location.protocol}//${window.location.hostname}`
+  const currentHost = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
 
   if (src.startsWith('https://images.pexels.com')) return src
   return `${currentHost}/${src}?w=${width}&q=${quality || 75}`
